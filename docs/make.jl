9e3590979dc1d2cb27431e010afe0fc42f101f83
Ben Elsen <mail@benelsen.com>
2019-11-12 00:10:03 +0100

    update documentation script

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,12 @@
 using Documenter, MQTT
 
-makedocs()
+makedocs(
+    sitename="MQTT",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    )
+)
+
+deploydocs(
+    repo = "github.com/benelsen/MQTT.jl.git",
+)
